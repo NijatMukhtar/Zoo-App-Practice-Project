35e5d81ed9bc940d50ae89e58d525c08669b1260
Nijat Mukhtarov <mukhog97@icloud.com>
2022-07-23 15:29:33 +0400

    First Creation

---
+++ Zoo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zoo App
+//
+//  Created by Nijat Mukhtarov on 23.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
